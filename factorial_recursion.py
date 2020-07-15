1c0a661bcd0a08f5a066e35f933dbdac216a89f5
Ankush Thakur <ankush.thakur53@gmail.com>
2020-07-15 20:46:49 +0530

    Add factorial code - tail and head recursion

---
+++ factorial_recursion.py
@@ -0,0 +1,17 @@
+# The factorial function implemented both as a head-call and tail-call recursion
+
+def factorial_head_call(n):
+    if n == 1:
+        return 1;
+    
+    return n * factorial_head_call(n-1)
+
+def factorial_tail_call(n, accumulator=1):
+    if n == 1:
+        return accumulator # as the accumulator contains the final result of all multiplications
+    
+    return factorial_tail_call(n-1, n * accumulator)
+
+if __name__ == "__main__":
+    print(factorial_head_call(5))
+    print(factorial_tail_call(5))
